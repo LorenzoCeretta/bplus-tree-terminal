1f64530e5b3078ce479ef7ce5b5afe592c1d8b54
Lorenzo Ceretta <lorenzopceretta@gmail.com>
2025-06-17 19:31:41 -0300

    Add black formatter to the project

--- bplustree.py
+++ bplustree.py
@@ -5,17 +5,19 @@
 # The order specifies the maximum number of children a node can have;
 # A leaf node in a B+ Tree of order (m) can hold a maximum of (m - 1) keys.
 
+
 class LeafNode:
-    '''
+    """
     Represents a leaf node that stores real data values;
     They are also connected with a doubly-linked list for efficient range queries;
     The m param is the order.
-    '''
+    """
+
     def __init__(self, m):
         self.keys = []
         self.values = []
-        self.next_leaf = None # Pointers to the next leaf in the linked list
-        self.prev_leaf = None # Pointers to the previous leaf in the linked list
+        self.next_leaf = None  # Pointers to the next leaf in the linked list
+        self.prev_leaf = None  # Pointers to the previous leaf in the linked list
         self.parent = None
         self.m = m
 
@@ -26,29 +28,34 @@ class LeafNode:
     def is_empty(self):
         return len(self.keys) == 0
 
+
 class InternalNode:
-    '''
+    """
     Represents an internal node that only stores keys for navigation;
     Internal nodes guide the search path to the appropriate leaf nodes;
     The m param is the order.
-    '''
+    """
+
     def __init__(self, m):
         self.keys = []
         self.children = []  # Pointers to child nodes
-        self.parent = None # Pointers to the parent node, useful for rebalancing
+        self.parent = None  # Pointers to the parent node, useful for rebalancing
         self.m = m
 
     def is_full(self):
+        # A leaf node is full if it has (m - 1) keys
         return len(self.keys) == self.m - 1
 
     def is_empty(self):
         return len(self.keys) == 0
 
+
 class BPlusTree:
-    '''
+    """
     B+ tree data structure optimized for database and file system applications.
     Provides efficient insertion, deletion, and range query operations.
-    '''
+    """
+
     def __init__(self, m):
-        self.m = m 
+        self.m = m
         self.root = LeafNode(m)
