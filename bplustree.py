28151c8fb8715e71f53f102c93d21e50bae60060
Lorenzo Ceretta <lorenzopceretta@gmail.com>
2025-06-17 11:54:05 -0300

    Add improves to basic BPlusTree and Nodes structure

--- bplustree.py
+++ bplustree.py
@@ -4,30 +4,36 @@
 # A leaf node in a B+ Tree or order (m) can hold a maximum of (m - 1) keys.
 
 class LeafNode:
-  def __init__(self):
-    self.data = []
+  def __init__(self, m):
+    self.keys = []
+    self.values = []
     self.next_leaf = None
     self.prev_leaf = None
+    self.parent = None
+    self.m = m
 
   def is_full(self, m):
     # m means the order of the B+ tree
     # A leaf node is full if it has (m - 1) keys
-    return len(self.keys) == m - 1
+    return len(self.keys) == self.m - 1
 
   def is_empty(self):
     return len(self.keys) == 0
 
 class InternalNode:
-  def __init__(self):
+  def __init__(self, m):
+    self.keys = []
     self.children = [] # Pointers to child notes
+    self.parent = None
+    self.m = m
 
   def is_full(self, m):
-    return len(self.keys) == m -1
+    return len(self.keys) == m - 1
 
   def is_empty(self):
     return len(self.keys) == 0
 
 class BPlusTree:
   def __init__(self, m):
-    self.root = LeafNode()
     self.m = m 
+    self.root = LeafNode(m)
