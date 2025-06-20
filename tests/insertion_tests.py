bf761bab3cfc24ff1010abfe52ff4047846157b4
Lorenzo Ceretta <lorenzopceretta@gmail.com>
2025-06-19 22:15:44 -0300

    Add doubly linked list test

--- tests/insertion_tests.py
+++ tests/insertion_tests.py
@@ -15,9 +15,9 @@ print(f"{tree.search_value(10)}")  # Should return Tom
 print(f"{tree.search_value(20)}")  # Should return Morgan
 print(tree)
 
-# 2) Testing first split
+# 2) First split test
 
-print("\n--- Testing First Split ---")
+print("\n--- First split test ---")
 
 tree.insert(15, "Robert")
 
@@ -28,23 +28,16 @@ print(f"{tree.search(20)}")
 print(tree)
 
 
-# 3) Testing second split
+# 3) Second split test
 
-print("\n--- Testing Second Split ---")
+print("\n--- Second split test ---")
 
 tree.insert(8, "Michael")
 tree.insert(9, "Hailee")
 tree.insert(11, "Jaden")
 tree.insert(12, "Matthew")
 
-print(f"{tree.search(8)}")
-print(f"{tree.search(9)}")
-print(f"{tree.search(10)}")
-print(f"{tree.search(11)}")
-print(f"{tree.search(12)}")
-print(f"{tree.search(15)}")
-print(f"{tree.search(20)}")
-
+tree.visualization()
 print(tree)
 
 # 4) Testing overwrite
@@ -53,3 +46,12 @@ print("\n--- Testing Overwrite ---")
 
 tree.insert(10, "Will")
 print(f"{tree.search_value(10)}")  # Should return Will
+
+# 5) Testing doubly linked list
+
+print("\n--- Testing Doubly Linked List ---")
+
+current_leaf = tree.search(8)  # Since 8 is the smallest key in this case
+while current_leaf:
+    print(f"Leaf: {current_leaf} -> {current_leaf.next_leaf}")
+    current_leaf = current_leaf.next_leaf
